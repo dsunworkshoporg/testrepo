fd6ed29219c7a4d9d44da725f45c5f95e64bf19f
dsunnerer <daniel@sunnerer.net>
2023-03-23 14:09:20 +0100

    adds password to code

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 def main():
     print("I wish you a very fine day")
 
+    supersecretpassword = "1234test1234"
+    print("Here is a super-secret password:", supersecretpassword)
+
 
 if __name__ == "__main__":
     main()
